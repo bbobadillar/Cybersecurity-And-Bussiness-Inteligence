fa95027e28d24ebd00d5618ba32aa5e5a760afd9
br1b0b <bbobadillar18@gmail.com>
2025-04-24 15:17:59 -0500

    add copyrigth and MIT license all ps1 - 2025-04-24 15:17:59

--- Cybersecurity/Automations/HexDump_PowerShell_Automation/Laboratory/M4LW4R3.bat
+++ Cybersecurity/Automations/HexDump_PowerShell_Automation/Laboratory/M4LW4R3.bat
@@ -1,3 +1,27 @@
+:: ================================================
+:: Simulated Malware Script [For Educational Use]
+:: ================================================
+
+:: © 2025 Bryan Bobadilla - Cybersecurity Specialist
+:: GitHub: https://github.com/bbobadillar
+:: LinkedIn: https://www.linkedin.com/in/bryan-bobadilla/
+
+:: Description:
+:: This batch file simulates basic malware behavior in a fully inoffensive and controlled way.
+:: It is intended for learning purposes to demonstrate, step-by-step, how a real attacker
+:: might interact with your folders, memory, or operating system under a malicious scenario.
+
+:: DISCLAIMER:
+:: ⚠️ THIS IS NOT MALWARE ⚠️
+:: This script does NOT cause harm. It is designed for cybersecurity training and analysis only.
+:: Do not use in production environments. Only execute in safe, isolated labs or virtual machines.
+
+:: Version: 1.0.0
+:: License: MIT (Non-commercial educational use only)
+
+:: ================================================
+
+
 @echo off
 REM -------------------------------------------------
 REM 🦠 Simulación de malware embebido en PNG
--- Cybersecurity/Automations/HexDump_PowerShell_Automation/Laboratory/SCRIPTFORINTORDUCEDM4LW4R3EMBEDEDTOIMAGE.ps1
+++ Cybersecurity/Automations/HexDump_PowerShell_Automation/Laboratory/SCRIPTFORINTORDUCEDM4LW4R3EMBEDEDTOIMAGE.ps1
@@ -1,3 +1,35 @@
+<#
+.SYNOPSIS
+    HexDump and Byte Extraction Tool
+
+.DESCRIPTION
+    This PowerShell script simulates the technique of embedding a batch file payload at the end of an image file.
+    The final bits of the image are appended with the batch file content in a way that remains visually                 undetectable.
+    
+    This method is often used in cybersecurity labs to study file obfuscation, steganography, and incident              response.
+    It is an inoffensive educational simulation and does NOT execute any malicious actions by itself.
+
+
+.AUTHOR
+    © 2025 Bryan Bobadilla (Cybersecurity & BI Specialist)
+    LinkedIn: https://www.linkedin.com/in/bryan-bobadilla/
+    GitHub: https://github.com/bbobadillar
+
+.NOTES
+    For educational use only. This tool is distributed for learning, personal R&D, and responsible testing.
+    Unauthorized use in production environments or against third-party systems is not permitted.
+
+.VERSION
+    1.0.0
+
+.LICENSE
+    MIT License (except for commercial redistribution)
+
+.ENJOY
+    Explore, learn, and analyze with passion. Stay curious and stay secure. ❤️🔐
+#>
+
+
 # 🧪 Usa rutas completas para evitar problemas
 $imagePath = "PUT YOUR FOLDER\IMG1.png"        # ⬅️ REEMPLAZA
 $scriptPath = "PUT YOUR FOLDER\M4LW4R3.bat"
